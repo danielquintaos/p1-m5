2c0586783fadd270674bc5b0d2aea56298bd348f
danielquintaos <daniel.davila@sou.inteli.edu.br>
2024-03-08 16:16:02 -0300

    commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# identificação do estudante
+
+daniel quintao davila
+
+
+# procedimento para executar o projeto 
+
+clonar o projeto
+`git clone https://github.com/danielquintaos/p1-mod5`
+
+criar o ambiente virtual
+`python -m venv venv`
+`source venv/bin/activate` (eu uso linux assim como Jesus Cristo)
+
+instalar as dependências
+`pip install -r requirements.txt`
+
+executar o projeto.
+`python app.py`
+
+
+# BREVE descrição dos diretórios e os arquivos que fazem parte do repositório. 
+
+app.py: funcao principal. determina rotas
+
+index.html: frontend. envia os comandos pra base de dados
+
+requirements.txt: pacotes necessarios para rodar o projeto
--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import request, jsonify
+from flask import render_template
+from flask import redirect, url_for
+from tinydb import TinyDB, Query
+
+
+app = Flask(__name__)
+db = TinyDB('caminhos.json')
+games_table = db.table('game')
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html', title='Welcome', message='Hello, Flask!')
+def index():
+	return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/<action>', methods = ['POST'])
+def action(action):
+	response = {
+	'action' : action
+	}
+
+	return render_template('index.html', **response)
